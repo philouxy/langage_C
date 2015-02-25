abd9f83e77d75f8b73736d4b985b2ec85418f353
Philou <ph.bovey@gmail.com>
2015-02-26 00:03:08 +0100

    Update main_langage_C_fixme.c

--- main_langage_C_fixme.c
+++ main_langage_C_fixme.c
@@ -1,8 +1,8 @@
 //-----------------------------------------------------------------------------------//
-// Nom du projet 		  :	      Langage_C_fixme
-// Nom du fichier 		:       main_langage_C_fixme.c
-// Date de création 	:       24.09.2013
-// Date de modification : 	  06.01.2015
+// Nom du projet 		:   Langage_C_fixme
+// Nom du fichier 		:   main_langage_C_fixme.c
+// Date de création 	:   24.09.2013
+// Date de modification : 	06.01.2015
 //
 // Auteur 				: 	Philou (Ph. Bovey)
 //
@@ -21,8 +21,12 @@
 //----------------------------------------------------------------------------------//
 
 //--- Librairie utilisées ---//
+//--- Standart ---//
 #include <stdio.h>                  // entrée - sortie
 #include <string.h>
+
+//--- Perso ---//
+#include "display.h"
 
 
 //--- Programme personalisé ---//
@@ -30,17 +34,21 @@ int main()
 {
     //--- Message de bienvenue ---//
     printf("--- Fixme - Exercices en C ! ---");
+    SAUT_LIGNE;
 
-    //--- Notion du "Printf" --> permet l'affichage dans une console ---//
-    printf("le printf : permet d'afficher du texte dans une console \n");
-    printf("-> le \\n permet un retour a la ligne \n");
-    printf("-> pour afficher un entier on utilise %%d \npour afficher une valeur en hexa %%x \n");
-    printf("-> pour un nombe a virgule on utilise %%f et pour notation scientifique on utilise %%E \n");
-    printf("-> pour aligner des nombres entier %%min.maxd definit les nb de caractere + un signe - devant le ");
-    printf("permet d'aligner a gauche \n");
-    printf("-> pour des nombres flottants %%car_total.chiffre_A_virgf définit les caracteres totaux a afficer");
-    printf("le nombre apres la virgule ");
-    printf("-> et pour afficher un caractere %%c \n\n");
+    //--- structure du menu ---//
+    Template_Menu_Ligne_Haut();
+
+    printf("1/ ... \n ");
+    printf("2/ ... \n ");
+    printf("3/ ... \n ");
+    printf("4/ ... \n ");
+    printf("--- help ---");
+    SAUT_LIGNE;
+    Help_Printf();
+
+    //--- structure du menu ---//
+    Template_Menu_Ligne_Bas();
 
     return 0;
 }
