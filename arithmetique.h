d01dcd9c66fe0cdf1f76fe1685fb3cc5fe1fa1df
Philou <ph.bovey@gmail.com>
2015-04-09 01:03:10 +0200

    Create arithmetique.h

---
+++ arithmetique.h
@@ -0,0 +1,28 @@
+//-----------------------------------------------------------------------------------//
+// Nom du projet 		: Langage_C_fixme
+// Nom du fichier 		: arithmetique.h
+// Date de création 	: 09.04.2015
+// Date de modification : xx.04.2015
+//
+// Auteur 				: Philou (Ph. Bovey)
+//
+// Description : fichier de type header file pour toutes les fonctions
+//               liées aux calculs
+//
+// Remarques :            lien pour la table ASCII :
+// 						  -> http://www.asciitable.com/
+// 						  lien pour la saisie de clavier avec getc & getchar pour ne pas
+//                        avoir des erreurs d'interprétation
+// 						  -> http://fr.openclassrooms.com/informatique/cours/utiliser-les-bonnes-fonctions-d-entree
+// 						  lien vers différents
+// 						  -> http://fr.wikipedia.org/wiki/String.h
+//----------------------------------------------------------------------------------//
+#ifndef ARITHMETIQUE_H_INCLUDED
+#define ARITHMETIQUE_H_INCLUDED
+
+
+//--- déclaration de prototype ---//
+int pemier_calcul(int val_x);
+
+
+#endif // ARITHMETIQUE_H_INCLUDED
