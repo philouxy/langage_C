b8e40ea28e1ca8bc23b90a63a5c60248f9c60be9
Philou <ph.bovey@gmail.com>
2015-06-23 18:16:21 +0200

    Update conversion.c

--- conversion.c
+++ conversion.c
@@ -2,7 +2,7 @@
 // Nom du projet 		: Langage_C_fixme
 // Nom du fichier 		: conversion.c
 // Date de création 	: 19.06.2015
-// Date de modification : xx.xx.2015
+// Date de modification : 23.06.2015
 //
 // Auteur 				: Philou (Ph. Bovey)
 //
@@ -26,10 +26,9 @@
 #include "display.h"
 
 
-
 //----------------------------------------------------------------------------------//
 // Nom de la fonction   : Conv_Char_Num
-// Entrée / Sortie      :  / - / -
+// Entrée / Sortie      : var_char / - / -
 // Description          : avec le choix de l'utilisateur sélectionnant un caractètre
 //                        la fonction affiche sa valeur decimale et hexa
 // modification         : le 19.06.2014
@@ -42,5 +41,22 @@ void Conv_Char_Num(char var_char)
     printf("conv en hexa : 0X%x", var_char);
     PRINT_SAUT_LIGNE;
     printf("conv en decimal :  %d", var_char);
+    PRINT_SAUT_LIGNE;
+}
+
+
+//----------------------------------------------------------------------------------//
+// Nom de la fonction   : Conv_Num_Char
+// Entrée / Sortie      : var_int / - / -
+// Description          : avec le choix de l'utilisateur sélectionnant un nombre
+//                        la fonction affiche la valeur en caractère
+// modification         : le 23.06.2014
+// Remarque             : -
+//----------------------------------------------------------------------------------//
+void Conv_Num_Char(int var_int)
+{
+    printf("la valeur selectionnee est '%d'", var_int);
+    PRINT_SAUT_LIGNE;
+    printf("conv en caractere : %c", var_int);
     PRINT_SAUT_LIGNE;
 }
