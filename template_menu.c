c1bdc40209812dd22ab1e25ab928d426e9084cfb
Philou <ph.bovey@gmail.com>
2015-08-20 00:06:34 +0200

    Update template_menu.c

--- template_menu.c
+++ template_menu.c
@@ -2,7 +2,7 @@
 // Nom du projet 		:   Langage_C_fixme
 // Nom du fichier 		:   template_menu.c
 // Date de création 	:   25.02.2015
-// Date de modification : 	19.06.2015
+// Date de modification : 	19.08.2015
 //
 // Auteur 				: 	Philou (Ph. Bovey)
 //
@@ -131,7 +131,7 @@ void Template_Choix_Menu_Principal(void)
 // Nom de la fonction   : Help_Printf
 // Entrée / Sortie      : - / - /
 // Description          : on affiche quelques régles avec le printf
-// Date modfification   : le 19.06.2015
+// Date modfification   : le 18.07.2015
 // Remarque             : -
 //----------------------------------------------------------------------------------//
 void Help_Printf(void)
@@ -139,6 +139,8 @@ void Help_Printf(void)
     //--- déclaration de variable interne ---//
     char var_test = 'A';
     float var_test2 = 3452.10;
+    int puissance = 5;
+    int i, val;
 
     //--- Notion du "Printf" --> permet l'affichage dans une console ---//
     printf("-> fct ""PRINTF()"" : permet d'afficher du texte dans une console \n");
@@ -159,10 +161,24 @@ void Help_Printf(void)
     printf("=> Exemple 2A : la variable de test vaut : %f \n", var_test2);
     printf("=> Exemple 2B : la variable de test vaut en notation scientifique : %3.1e \n", var_test2);
     PRINT_SAUT_LIGNE;
+    PRINT_SAUT_LIGNE;
+    printf("-> Valeur de fin de fichier dit EOF : %d", EOF);
+    PRINT_SAUT_LIGNE;
+    printf("-> allignement de nombres entier %%nb_esapce+indicateur \n");
+    printf("-> les signes  + & - devant le nb d'espace definit les sens d'alignement gauche ou droite \n");
+    PRINT_SAUT_LIGNE;
+    printf("=> Exemple 3 : allignement de nbs - de 1 a 10^5 \n");
+    for(i = 0 ; i < puissance + 1 ; i++)
+    {
+        //--- calcul de la valeur à afficher ---//
+        if(i == 0)
+            val = 1;
+        else
+            val = val*10;
 
-    printf("-> pour aligner des nombres entier %%min.max \n   definit les nb de caractere + un signe - devant le ");
-    printf("   permet d'aligner a gauche \n");
-    printf("-> pour des nombres flottants %%car_total.chiffre_A_virgf \n   définit les caracteres totaux a afficer");
-    printf("   le nombre apres la virgule ");
+        //--- affichage de la valeur avec alignement de droite a gauche ---//
+        printf("puissance %d -> %6d || %-6d", i, val, val);
+        PRINT_SAUT_LIGNE;
+    }
 
 }
