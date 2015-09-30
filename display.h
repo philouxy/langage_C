f8cb0e99130fadf539cd3109a09a158703c309ee
Philou <ph.bovey@gmail.com>
2015-10-01 00:42:25 +0200

    Update display.h

--- display.h
+++ display.h
@@ -2,7 +2,7 @@
 // Nom du projet 		    : Langage_C_fixme
 // Nom du fichier 		    : display.h
 // Date de création 	    : 25.02.2015
-// Date de modification     : 19.08.2015
+// Date de modification     : 30.09.2015
 //
 // Auteur 				    : Philou (Ph. Bovey)
 //
@@ -29,6 +29,7 @@
 
 #define PRINT_SAUT_LIGNE        printf("\n")
 #define PRINT_ESPACE            printf(" ")
+#define PRINT_ETOILE            printf("*");
 #define PRINT_M_MENU            printf("MENU : ")
 #define PRINT_SELECTION_MENU    printf("Veuillez Selectionner votre Menu : ")
 #define PRINT_SELECTION_CHAR    printf("Veuillez taper un caractere :")
@@ -38,10 +39,12 @@
 #define PRINT_SELECTION_FLOAT   printf("Voulez-vous travailler en flottant - taper 'F' ")
 
 #define NAME_M_ADITION              "ADITION"
+#define NAME_M_AFFICHAGE            "AFFICHAGE"
 #define NAME_M_ARITHEMTIQUE         "ARITHMETIQUE"
 #define NAME_M_ASTUCE_PRINTF        "ASTUCE PRINTF"
 #define NAME_M_ASTUCE_INCR          "ASTUCE INCREMENTATION & DECREMENTATION"
 #define NAME_M_ASTUCE_CONV_CHAR_NUM "ASTUCE CONVERSION CARACTERE NUMERIQUE - TABLE"
+#define NAME_M_ASTUCE_LOOP          "ASTUCE BOUCLE"
 #define NAME_M_CONV                 "CONVERSIONS"
 #define NAME_M_CONV_CHAR_NUM        "CONVERSION CARACTERE EN NUM (DEC - HEXA)"
 #define NAME_M_CONV_NUM_CHAR        "CONVERSION NUMERIQUE EN CARACTERE"
@@ -60,7 +63,7 @@ extern char var_retour;
 void Template_Menu_Ligne(void);                         //-> affiche un trait d'*
 void Template_Display_Menu(char choix_menu);            //-> affiche le titre dans lequel se trouve le user et sélectionne le menu choisi
 void Template_Choix_Menu_Principal(void);               //-> affiche le menu principal pour le user
-void Help_Printf(void);                                 //-> affiche une aide sur le printf
+
 
 
 //--- liée au fichier template_menu_arithm.c ---//
@@ -78,6 +81,14 @@ char Select_Option(void);                               //-> permet à l'utilist
 float Select_Var_Flottant(void);                        //-> récupère une chaine de caractère et la transforme en floattant
 int Select_Var_Entier(void);                            //-> récupère une châine de chaîne de caractère et le transforme en entier
 
+//--- lié au fichier help.c ---//
+void Help_Printf(void);                                 //-> affiche une aide sur le printf
+void Help_Loop(void);                                   //-> effect visuel avec des boucles itératives
+
+//--- liée au fichier affichage.c ---//
+void Triangle_Etoile_AGB(void);                         // permet d'afficher un triangle selon les datas du user
+void Triangle_Etoile_AGH(void);
+void Triangle_Etoile_ADB(void);
 
 
 #endif // DISPLAY_H_INCLUDED
