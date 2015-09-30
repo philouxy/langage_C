18ef862a0e5090a6570c8a13d2febdc7298c8782
Philou <ph.bovey@gmail.com>
2015-10-01 00:42:54 +0200

    Update template_menu.c

--- template_menu.c
+++ template_menu.c
@@ -2,7 +2,7 @@
 // Nom du projet 		:   Langage_C_fixme
 // Nom du fichier 		:   template_menu.c
 // Date de création 	:   25.02.2015
-// Date de modification : 	09.09.2015
+// Date de modification : 	30.09.2015
 //
 // Auteur 				: 	Philou (Ph. Bovey)
 //
@@ -46,7 +46,7 @@ void Template_Menu_Ligne(void)
 // Nom de la fonction   : Template_Display_Menu
 // Entrée / Sortie      : choix_menu / - /
 // Description          : affiche le menu dans lequel on se situe
-// Date modfification   : le 17.06.2015
+// Date modfification   : le 30.06.2015
 // Remarque             : -
 //----------------------------------------------------------------------------------//
 void Template_Display_Menu(char choix_menu)
@@ -65,7 +65,7 @@ void Template_Display_Menu(char choix_menu)
 
     switch(choix_menu)
     {
-        case('P'):
+        case 'P':
             printf("%s", NAME_M_PRINICIPAL);
             PRINT_SAUT_LIGNE;
             Template_Menu_Ligne();
@@ -73,7 +73,7 @@ void Template_Display_Menu(char choix_menu)
             Template_Choix_Menu_Principal();
         break;
 
-        case('1'):
+        case '1':
             printf("%s", NAME_M_ARITHEMTIQUE);
             PRINT_SAUT_LIGNE;
             Template_Menu_Ligne();
@@ -86,12 +86,30 @@ void Template_Display_Menu(char choix_menu)
             PRINT_SAUT_LIGNE;
         break;
 
-        case('A'):
+        case '2':
+            printf("%s", NAME_M_AFFICHAGE);
+            PRINT_SAUT_LIGNE;
+            Template_Menu_Ligne();
+            PRINT_SAUT_LIGNE;
+            Triangle_Etoile_AGH();
+            Triangle_Etoile_AGB();
+            Triangle_Etoile_ADB();
+        break;
+
+        case 'A':
             printf("%s", NAME_M_ASTUCE_PRINTF);
             PRINT_SAUT_LIGNE;
             Template_Menu_Ligne();
             PRINT_SAUT_LIGNE;
             Help_Printf();
+        break;
+
+        case 'B':
+            printf("%s", NAME_M_ASTUCE_LOOP);
+            PRINT_SAUT_LIGNE;
+            Template_Menu_Ligne();
+            PRINT_SAUT_LIGNE;
+            Help_Loop();
         break;
 
         default:
@@ -106,7 +124,7 @@ void Template_Display_Menu(char choix_menu)
 // Nom de la fonction   : Template_Choix_Menu
 // Entrée / Sortie      : - / - /
 // Description          : affiche le choix du menu pour l'utilisateur
-// Date modfification   : le 17.06.2015
+// Date modfification   : le 24.09.2015
 // Remarque             : -
 //----------------------------------------------------------------------------------//
 void Template_Choix_Menu_Principal(void)
@@ -116,73 +134,17 @@ void Template_Choix_Menu_Principal(void)
 
     printf("[1] %s", NAME_M_ARITHEMTIQUE);
     PRINT_SAUT_LIGNE;
-    printf("[2] ... \n");
+    printf("[2] %s", NAME_M_AFFICHAGE);
+    PRINT_SAUT_LIGNE;
     printf("[3] ... \n");
     printf("[4] ... \n");
     printf("[A] --- %s ---", NAME_M_ASTUCE_PRINTF);
     PRINT_SAUT_LIGNE;
+    printf("[B] --- %s ---", NAME_M_ASTUCE_LOOP);
+    PRINT_SAUT_LIGNE;
     printf("[Q] --- help ---\n");
     printf("[E] --- fermer le programme ---");
-
-    //return
 }
-
-//----------------------------------------------------------------------------------//
-// Nom de la fonction   : Help_Printf
-// Entrée / Sortie      : - / - /
-// Description          : on affiche quelques régles avec le printf
-// Date modfification   : le 09.09.2015
-// Remarque             : -
-//----------------------------------------------------------------------------------//
-void Help_Printf(void)
-{
-    //--- déclaration de variable interne ---//
-    char var_test = 'A';
-    float var_test2 = 3452.10;
-    int puissance = 5;
-    int i, val;
-
-    //--- Notion du "Printf" --> permet l'affichage dans une console ---//
-    printf("-> fct ""PRINTF()"" : permet d'afficher du texte dans une console \n");
-    printf("-> le \\n permet un retour a la ligne \n");
-    printf("-> pour afficher un caractere on utilise %%c \n");
-    printf("-> pour afficher un entier on utilise %%d \n");
-    printf("-> pour afficher une valeur en hexa on utilise %%x \n");
-    PRINT_SAUT_LIGNE;
-    printf("=> Exemple 1A : la variable de test a comme caractere : %-3c \n", var_test);
-    printf("=> Exemple 1B : la variable de test vaut en decimal :   %-3d \n", var_test);
-    printf("=> Exemple 1C : la variable de test vaut en hexa :    OX%-3x \n", var_test);
-    PRINT_SAUT_LIGNE;
-    printf("-> pour afficher un nombre flottant on utilise %%f \n");
-    printf("-> pour afficher un nombre en notation scientifique on utilise %%e \n");
-    PRINT_SAUT_LIGNE;
-    printf("REMARQUE : ecriture notation scientifique %%nb_total.Nb_ap_virgule => ex: %%3.1e \n");
-    PRINT_SAUT_LIGNE;
-    printf("=> Exemple 2A : la variable de test vaut : %f \n", var_test2);
-    printf("=> Exemple 2B : la variable de test vaut en notation scientifique : %3.1e \n", var_test2);
-    PRINT_SAUT_LIGNE;
-    PRINT_SAUT_LIGNE;
-    printf("-> Valeur de fin de fichier dit EOF : %d", EOF);
-    PRINT_SAUT_LIGNE;
-    printf("-> allignement de nombres entier %%nb_esapce+indicateur \n");
-    printf("-> les signes  + & - devant le nb d'espace definit les sens d'alignement gauche ou droite \n");
-    PRINT_SAUT_LIGNE;
-    printf("=> Exemple 3 : allignement de nbs - de 1 a 10^5 \n");
-    for(i = 0 ; i < puissance + 1 ; i++)
-    {
-        //--- calcul de la valeur à afficher ---//
-        if(i == 0)
-            val = 1;
-        else
-            val = val*10;
-
-        //--- affichage de la valeur avec alignement de droite a gauche ---//
-        printf("puissance %d -> %6d || %-6d", i, val, val);
-        PRINT_SAUT_LIGNE;
-    }
-}
-
-
 
     //printf("\x1b[1;1 H]");
     //system("cls");
