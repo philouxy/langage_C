8abbe437aafac070705c966041f682799324fef6
Philou <ph.bovey@gmail.com>
2015-10-08 15:25:35 +0200

    Update affichage.c
    the function Triangle_Etoile_AGH don't work for the moment

--- affichage.c
+++ affichage.c
@@ -2,7 +2,7 @@
 // Nom du projet 		: Langage_C_fixme
 // Nom du fichier 		: affichage.c
 // Date de création 	: 30.09.2015
-// Date de modification : XX.XX.2015
+// Date de modification : 08.10.2015
 //
 // Auteur 				: Philou (Ph. Bovey)
 //
@@ -55,7 +55,7 @@ void Triangle_Etoile_AGB(void)
 // Nom de la fonction   : Triangle_Etoile_ADB
 // Entrée / Sortie      : - / - /
 // Description          : affiche un triangle rectangle selon le choix du user
-// Date modfification   : le 09.09.2015
+// Date modfification   : le 08.10.2015
 // Remarque             : -
 //----------------------------------------------------------------------------------//
 void Triangle_Etoile_ADB(void)
@@ -74,13 +74,16 @@ void Triangle_Etoile_ADB(void)
                 PRINT_ESPACE;
             else
             {
-                for(c_e = c; c_e < val_user; c_e++)
+                for(c_e = c; c_e < (val_user + l); c_e++)
                     PRINT_ETOILE;
             }
         }
         PRINT_SAUT_LIGNE;
     }
 }
+
+//for(c_e = c; c_e < (val_user + c); c_e++)
+
 
 //----------------------------------------------------------------------------------//
 // Nom de la fonction   : Triangle_Etoile_AGH
@@ -89,11 +92,10 @@ void Triangle_Etoile_ADB(void)
 // Date modfification   : le 09.09.2015
 // Remarque             : -
 //----------------------------------------------------------------------------------//
-
 void Triangle_Etoile_AGH(void)
 {
     //--- déclaration de variable interne ---//
-    int c, l;       // c = colonne || l = ligne
+    int c, l;       // c = colonne || l = ligne 
     int val_user;
 
     val_user = Select_Var_Entier();
@@ -101,6 +103,33 @@ void Triangle_Etoile_AGH(void)
     for(l = 0; l < val_user; l++)
     {
         for(c = l; c < val_user ; c++)
+            PRINT_ETOILE;
+
+        PRINT_SAUT_LIGNE;
+    }
+}
+
+//----------------------------------------------------------------------------------//
+// Nom de la fonction   : Triangle_Etoile_AGH
+// Entrée / Sortie      : - / - /
+// Description          : affiche un triangle rectangle selon le choix du user
+// Date modfification   : le 08.10.2015
+// Remarque             : -
+//----------------------------------------------------------------------------------//
+void Triangle_Etoile_ADH(void)
+{
+    //--- déclaration de variable interne ---//
+    int c, c_e, l;       // c = colonne || l = ligne || c_e = colonne étoile
+    int val_user;
+
+    val_user = Select_Var_Entier();
+
+    for(l = 0; l < val_user; l++)
+    {
+        for(c = l, c_e = val_user; c < val_user ; c++)
+        {
+            
+        }
             PRINT_ETOILE;
 
         PRINT_SAUT_LIGNE;
