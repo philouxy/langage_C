21ed095eea61644d5f2c3b008b60195193c52707
Philou <ph.bovey@gmail.com>
2015-11-06 14:03:57 +0100

    Create template_menu_display.c

---
+++ template_menu_display.c
@@ -0,0 +1,112 @@
+//-----------------------------------------------------------------------------------//
+// Nom du projet 		:   Langage_C_fixme
+// Nom du fichier 		:   template_menu_display.c
+// Date de création 	:   06.11.2015
+// Date de modification : 	xx.xx.2015
+//
+// Auteur 				: 	Philou (Ph. Bovey)
+//
+// Description 			: 	Sorte de template modifiable pour afficher un menu ->
+//                          ici la partie du menu display pour afficher des formes
+//
+// Remarques			:   lien pour la table ASCII :
+//                          http://www.asciitable.com/
+//                          lien pour la saisie de clavier avec getc & getchar pour ne pas
+//                          avoir des erreurs d'interprétation
+//                          http://fr.openclassrooms.com/informatique/cours/utiliser-les-bonnes-fonctions-d-entree
+//                          lien vers différents
+//                          http://fr.wikipedia.org/wiki/String.h
+//----------------------------------------------------------------------------------//
+
+//--- librairie standart ---//
+#include <stdio.h>                  // entrée - sortie
+#include <string.h>
+
+//--- librairie perso ---//
+#include "display.h"
+//#include "arithmetique.h"
+
+//----------------------------------------------------------------------------------//
+// Nom de la fonction   : Template_Choix_Menu_Display
+// Entrée / Sortie      : - / - /
+// Description          : affiche le choix du menu "Display"
+// Date modfification   : le 06.11.2015
+// Remarque             : -
+//----------------------------------------------------------------------------------//
+void Template_Choix_Menu_Display(void)
+{
+    printf("[1] %s", NAME_M_DISP_CARRE);
+    PRINT_SAUT_LIGNE;
+    printf("[2] %s", NAME_M_DISP_TRIANGLE);
+    PRINT_SAUT_LIGNE;
+    printf("[3] %s", NAME_M_DISP_LOSANGE);
+    PRINT_SAUT_LIGNE;
+    printf("[4] ... \n");
+    printf("[P] --- retour menu %s  ---", NAME_M_PRINICIPAL);
+    PRINT_SAUT_LIGNE;
+    printf("[E] --- fermer le programme ---");
+}
+
+
+//----------------------------------------------------------------------------------//
+// Nom de la fonction   : Template_Display_Menu_Display
+// Entrée / Sortie      : choix_menu / - /
+// Description          : affiche le menu dans lequel on se situe -> ici display
+// Date modfification   : le 06.11.2015
+// Remarque             : -
+//----------------------------------------------------------------------------------//
+void Template_Display_Menu_Display(char choix_menu)
+{
+    //--- déclaration de variable interne ---//
+    char i, var_retour;
+
+    //--- affiche une ligne entière d'étoile ---//
+    PRINT_SAUT_LIGNE;
+    Template_Menu_Ligne();
+
+    //--- boucle pour afficher x caractère sur une ligne ---//
+    for(i = 0; i < 3; i++)
+        printf("*");
+
+    //--- pour l'esthétique afficher un espace avant le mots "menu" ---/
+    PRINT_ESPACE;
+    PRINT_M_MENU;
+
+    switch(choix_menu)
+    {
+        case 'P':
+            printf("%s", NAME_M_PRINICIPAL);
+            PRINT_SAUT_LIGNE;
+            Template_Menu_Ligne();
+            PRINT_SAUT_LIGNE;
+            Template_Choix_Menu_Principal();
+        break;
+
+        case '1':
+            printf("%s", NAME_M_DISP_CARRE);
+            PRINT_SAUT_LIGNE;
+            Template_Menu_Ligne();
+            PRINT_SAUT_LIGNE;
+        break;
+
+        case '2':
+            printf("%s", NAME_M_DISP_TRIANGLE);
+            PRINT_SAUT_LIGNE;
+            Template_Menu_Ligne();
+            PRINT_SAUT_LIGNE;
+        break;
+
+        case '3':
+            printf("%s", NAME_M_DISP_LOSANGE);
+            PRINT_SAUT_LIGNE;
+            Template_Menu_Ligne();
+            PRINT_SAUT_LIGNE;
+        break;
+
+        default:
+            printf("LA SELECTION N'EXISTE PAS !!!");
+            PRINT_SAUT_LIGNE;
+            Template_Menu_Ligne();
+        break;
+    }
+}
