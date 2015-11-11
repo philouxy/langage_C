2a5f41b7e14fc770e8437e1a16c19b21ff77e041
Philou <ph.bovey@gmail.com>
2015-11-11 14:02:45 +0100

    Update display.h

--- display.h
+++ display.h
@@ -2,7 +2,7 @@
 // Nom du projet 		    : Langage_C_fixme
 // Nom du fichier 		    : display.h
 // Date de création 	    : 25.02.2015
-// Date de modification     : 09.11.2015
+// Date de modification     : 11.11.2015
 //
 // Auteur 				    : Philou (Ph. Bovey)
 //
@@ -25,7 +25,10 @@
 //--- constante ou définition ---//
 #define MAX_CARA                80
 #define CHOIX_ENTIER            73          // représente I en caractère
+#define CHOIX_EXIT              69          // représente E en caractère
 #define CHOIX_FLOTTANT          70          // représente F en caractère
+#define CHOIX_MENU              77          // représente M en caractère
+#define CHOIX_MENU_PRINCIPAL    80          // représente P en caractère
 
 #define PRINT_SAUT_LIGNE        printf("\n")
 #define PRINT_ESPACE            printf(" ")
@@ -38,25 +41,26 @@
 #define PRINT_SELECTION_INT     printf("Voulez-vous travailler en entier   - taper 'I' ")
 #define PRINT_SELECTION_FLOAT   printf("Voulez-vous travailler en flottant - taper 'F' ")
 
-#define NAME_M_ADITION              "ADITION"
-#define NAME_M_AFFICHAGE            "AFFICHAGE"
-#define NAME_M_ARITHEMTIQUE         "ARITHMETIQUE"
-#define NAME_M_ASTUCE_PRINTF        "ASTUCE PRINTF"
-#define NAME_M_ASTUCE_INCR          "ASTUCE INCREMENTATION & DECREMENTATION"
-#define NAME_M_ASTUCE_CONV_CHAR_NUM "ASTUCE CONVERSION CARACTERE NUMERIQUE - TABLE"
-#define NAME_M_ASTUCE_LOOP          "ASTUCE BOUCLE"
-#define NAME_M_CONV                 "CONVERSIONS"
-#define NAME_M_CONV_CHAR_NUM        "CONVERSION CARACTERE EN NUM (DEC - HEXA)"
-#define NAME_M_CONV_NUM_CHAR        "CONVERSION NUMERIQUE EN CARACTERE"
-#define NAME_M_CONV_NUM_OCT_HEX     "CONVERSION NUMERIQUE EN OCTAL - HEXA"
-#define NAME_M_DIVISION             "DIVISION"
-#define NAME_M_DISP_CARRE           "AFFICHAGE DE CARRE"
-#define NAME_M_DISP_TRIANGLE        "AFFICHAGE DE TRIANGLE"
-#define NAME_M_DISP_LOSANGE         "AFFICHAGE DE LOSANGE"
-#define NAME_M_MULTIPLICATION       "MULTIPLICATION"
-#define NAME_M_OP_SIMPLE            "OPERATION SIMPLE"
-#define NAME_M_PRINICIPAL           "PRINCIPAL"
-#define NAME_M_SOUSTRACTION         "SOUSTRACTION"
+#define NAME_M_ADITION                  "ADITION"
+#define NAME_M_AFFICHAGE                "AFFICHAGE"
+#define NAME_M_ARITHEMTIQUE             "ARITHMETIQUE"
+#define NAME_M_ASTUCE_PRINTF            "ASTUCE PRINTF"
+#define NAME_M_ASTUCE_INCR              "ASTUCE INCREMENTATION & DECREMENTATION"
+#define NAME_M_ASTUCE_CONV_CHAR_NUM     "ASTUCE CONVERSION CARACTERE NUMERIQUE - TABLE"
+#define NAME_M_ASTUCE_LOOP              "ASTUCE BOUCLE"
+#define NAME_M_CONV                     "CONVERSIONS"
+#define NAME_M_CONV_CHAR_NUM            "CONVERSION CARACTERE EN NUM (DEC - HEXA)"
+#define NAME_M_CONV_NUM_CHAR            "CONVERSION NUMERIQUE EN CARACTERE"
+#define NAME_M_CONV_NUM_OCT_HEX         "CONVERSION NUMERIQUE EN OCTAL - HEXA"
+#define NAME_M_DIVISION                 "DIVISION"
+#define NAME_M_DISP_CARRE               "AFFICHAGE DE CARRE"
+#define NAME_M_DISP_LOSANGE             "AFFICHAGE DE LOSANGE"
+#define NAME_M_DISP_PARALLELOGRAMME     "AFFICHAGE DE PARALLELOGRAMME"
+#define NAME_M_DISP_TRIANGLE            "AFFICHAGE DE TRIANGLE"
+#define NAME_M_MULTIPLICATION           "MULTIPLICATION"
+#define NAME_M_OP_SIMPLE                "OPERATION SIMPLE"
+#define NAME_M_PRINICIPAL               "PRINCIPAL"
+#define NAME_M_SOUSTRACTION             "SOUSTRACTION"
 
 //--- déclaration de variable global ---//
 extern char var_retour;
@@ -79,7 +83,6 @@ void Template_Display_Menu_Conversion(char choix_menu); //-> affiche le menu con
 void Template_Choix_Menu_Display(void);                 //-> affichage de différents theme du menu display
 void Template_Display_Menu_Display(char choix_menu);    //-> affiche le titre dans lequel se trouve le user et sélectionne le menu choisi
 
-
 //--- liée au fichier selection_user.c ---//
 char Selection_Menus(char choix_selection_msg);         //-> permet de récuprer une valeur entrer par l'utilisateur
 char Select_Var_Cara(void);                             //-> récupère un caractère fournis par un utilisateur
@@ -97,6 +100,7 @@ void Triangle_Etoile_AGH(void);
 void Triangle_Etoile_ADB(void);
 void Triangle_Etoile_ADH(void);
 void Carre(int val_user);                               // afficher un carré plein d'étoiles
+void Parallelogramme_AG(int val_user);                  // affiche un parallelogramme avec l'angle a gauche
 
 
 
