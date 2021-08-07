bb6b3419c9811e8a267100d9a81080e372892235
Philou <ph.bovey@gmail.com>
2021-08-08 00:31:32 +0200

    Update Demo1.c
    just display a message on alias shell

--- DemoCours/Demo1.c
+++ DemoCours/Demo1.c
@@ -1,1 +1,24 @@
-// code source de petite demo en C 
+//-----------------------------------------------------------------------------------//
+// Nom du projet 		: Demo Cours en C 
+// Nom du fichier 		: DemoC1.c
+// Date de création 	: 07.08.2021
+// Date de modification : xx.xx.2021
+//
+// Auteur 				: Philou (Ph. Bovey)
+//
+// Description          : Juste un "hello world" 
+//
+//
+// Remarques :           
+//----------------------------------------------------------------------------------//
+//-- librairie standart --//
+#include <stdio.h>				// appel de la librairie pour les I/O 
+
+//-- librairie perso --//
+
+//-- entrée du programme => main --// 
+void main(void)
+{
+	//-- affichage d'un msg en mode console --// 
+	printf("hello les SOL2 !"); 
+}
